1e05e9755b358d8a9a641e7ef132ab4de7875c71
fanlizhong <fanlizhong@mioji.com>
2016-11-01 12:09:29 +0800

    add example

---
+++ demo/example/webpack.config.js
@@ -0,0 +1,43 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: './build/',
+    filename: "index.js"
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+          test: /\.css$/,
+          loader:ExtractTextPlugin.extract('style', 'css?sourceMap!')
+      },
+      // {
+      //   test: /\.(ttf|eot|svg|woff(2))(\?[a-z0-9]+)?$/,
+      //   loader : 'file-loader'
+      // }, 
+      {
+          test: /\.scss$/,
+          loader:ExtractTextPlugin.extract('style', 'css?sourceMap!sass?sourceMap!url')
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new ExtractTextPlugin('bundle.css',{
+      allChunks: true
+    })
+  ]
+};
